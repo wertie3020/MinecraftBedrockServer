fe30bb7b5f48811143568dd8718f04df5468d7c9
wertie <wertie@pop-os.localdomain>
2022-07-08 10:25:13 +0200

    change url for update

--- update.sh
+++ update.sh
@@ -17,4 +17,4 @@ if [[ $(id -u) = 0 ]]; then
    exit 1
 fi
 
-curl https://raw.githubusercontent.com/TheRemote/MinecraftBedrockServer/master/SetupMinecraft.sh | bash
+curl https://raw.githubusercontent.com/$github_user/MinecraftBedrockServer/$github_branch/SetupMinecraft.sh | bash
